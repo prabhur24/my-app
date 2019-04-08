f4679d175b9a65e1cdbb8ce5ba3d453d478a92c3
Prabhu <prabhu@technogramsolutions.com>
2019-04-08 23:51:10 +0530

    changes for first application;

---
+++ src/counter.jsx
@@ -0,0 +1,18 @@
+import React,{Component} from 'react';
+
+export default class Counter extends Component{
+    state = {
+        value:1
+    };
+
+    render(){
+        return(
+        <div> 
+        <div>{ this.state.value }</div>
+        <button onClick={() => { this.setState({
+            value:this.state.value +1
+        }) }}>Increment</button>
+        </div>
+        );
+    }
+}
